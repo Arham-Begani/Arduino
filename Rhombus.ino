a64857215f48b17332268a8cc3a4a5bdda979945
Arham Begani <123586002+Arham-Begani@users.noreply.github.com>
2025-08-01 17:58:03 +0530

    Added the Rhombus code
    This code makes the robot make a rhombus.

---
+++ Rhombus.ino
@@ -0,0 +1,23 @@
+// Rhombus
+
+a1 = 600;
+a2 = 1200;
+x = 800;
+
+moveForward(x);
+stopMotor();
+turnRight(a1);
+
+moveForward(x);
+stopMotor();
+turnRight(a2);
+
+moveForward(x);
+stopMotor();
+turnRight(a1);
+
+moveForward(x);
+stopMotor();
+turnRight(a2);
+}
+
